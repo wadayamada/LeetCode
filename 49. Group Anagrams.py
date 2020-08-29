bd5891e95ac19a5fc983e7afd5faf212b0552959
Takamichi Wada <31792905+wadayamada@users.noreply.github.com>
2020-08-29 18:36:34 +0900

    Create 49. Group Anagrams.py

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,11 @@
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        
+        result=defaultdict(list)
+        for s in strs:
+            result[tuple(sorted(s))].append(s)
+            
+        return result.values()
